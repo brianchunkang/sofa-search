1ba37c7a2edcb90e9679e478e03dcd3001820054
Nicholas Vadivelu <nick@bassoft.com>
2017-09-16 15:49:40 -0400

    started integrating ml into flask

--- index.py
+++ index.py
@@ -42,7 +42,6 @@ def display(img):
 
 X_test = np.empty(shape=(target_width,target_height,3,0)) #needs optimization
 for fname in glob.glob(path):
-	print(fname)
 	im, pixels = load_image(fname)
 	X_test = np.append(X_test, pixels, axis=3)
 	numImages = numImages + 1
@@ -68,44 +67,49 @@ model.compile(loss='mean_squared_error',
 def train(X,Y):
 	model.fit(X, Y, batch_size=1, nb_epoch=10)
 
-count = 0
-overallMax = 0
-overallImg = 0
-list = [None]*lim
-
 def getInput():
 	return input('Score: ')
-
-while count<lim:
-	display(X_train)
-	Y_train = np.full((1,1),getInput())
-	train(X_train[np.newaxis,...],Y_train)
-	max_thing = 0
-	ind = 0
-	for i in range(0,X_test.shape[3]):
-		if i not in list:
-			img = X_test[:,:,:,i]
-			score = model.evaluate(img[np.newaxis,...], np.zeros((1,1)), verbose=1)
-			if score>max_thing:
-				max_thing = score
-				X_train = img
-				ind = i
-			if score>overallMax:
-				overallMax = score
-				overallImg = img
-	if score>threshold:
-		display(X_train)
-	list[count] = ind
-	count = count+1
-
-display(overallImg)
 
 @app.route('/',methods=['GET','POST'])
 def index():
 	if request.method=='POST':
-		
 	else:
         return render_template('sofa.html')
+
+@app.route('/submission/<num>', methods=['POST'])
+def rating(num=-1):
+	if num == -1:
+		break
+		
+	count = 0
+	overallMax = 0
+	overallImg = 0
+	list = [None]*lim
+
+	while count<lim:
+		display(X_train)
+		Y_train = np.full((1,1),num)
+		train(X_train[np.newaxis,...],Y_train)
+		max_thing = 0
+		ind = 0
+		for i in range(0,X_test.shape[3]):
+			if i not in list:
+				img = X_test[:,:,:,i]
+				score = model.evaluate(img[np.newaxis,...], np.zeros((1,1)), verbose=1)
+				if score > max_thing:
+					max_thing = score
+					X_train = img
+					ind = i
+				if score>overallMax:
+					overallMax = score
+					overallImg = img
+		if score>threshold:
+			display(X_train)
+		list[count] = ind
+		count = count+1
+
+	display(overallImg)
+	
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
