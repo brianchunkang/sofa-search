6d485a26ffee75e1af4533bc2a40a7edab53f161
Nicholas Vadivelu <nick@bassoft.com>
2017-09-16 11:59:15 -0400

    finished web scraping v1

---
+++ image-scraper.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen, urlretrieve
+from bs4 import BeautifulSoup
+import os
+
+url = "http://www.ikea.com/ca/en/catalog/categories/departments/living_room/10662/"
+html = urlopen(url)
+soup = BeautifulSoup(html, "lxml")
+
+imgs = soup.findAll("div", {"class":"threeColumn product "})
+
+counter = 0
+information = []
+
+for img in imgs:
+    url2 = "http://www.ikea.com"+img.a['href']
+    print(url2)
+    html2 = urlopen(url2)
+    soup2 = BeautifulSoup(html2, "lxml");
+    img_needed = soup2.find("div", {"class":"rightContentContainer"})
+
+    img_url = "http://www.ikea.com" + img_needed.img['src']
+
+    urlretrieve(img_url, os.path.basename("sofa_" + str(counter) + ".jpg"))
+
+    counter += 1;
+
+    information.append({url2, soup2.find("div", {"class":"productName"}).decode_contents(formatter="html").strip()})
+
+print(information)
