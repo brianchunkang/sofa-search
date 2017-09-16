9555112c40c8f44e9625c7c2244dc88a836c05e9
VictorHYu <victor.yu.canada@gmail.com>
2017-09-16 18:46:13 -0400

    setup local environment, update dependencies, reorg backend code

--- index.py
+++ index.py
@@ -12,58 +12,27 @@ from PIL import Image
 import glob
 app = Flask(__name__)
 
-path = "sofa-folder/*"
+# constants
+path = "scraped-sofas/*"
 target_width = 64
 target_height = 64
 
-#Returns pixel data as well as width 
-def load_image( filename ):
-	im = Image.open( filename )
-	im = im.resize((target_width,target_height),Image.ANTIALIAS)
-	pixels = np.asarray(im.getdata())
-	#pixels = pixels/255.0*2 - 1
-	pixels = np.resize(pixels, (im.height, im.width, 3, 1)) #need the extra dimension to stack later
-	return im, pixels
-
-im,_ = load_image('sofa-folder/sofa.jpg')
-
-img_width = im.width
-img_height = im.height
 img_depth = 3
 threshold = 0.9
 lim = 5
 numImages = 0
 
-def display(img):   
-	return Image.fromarray(img.squeeze().astype('uint8'), 'RGB')
+# Returns pixel data as well as width
+def load_image( filename ):
+    im = Image.open( filename )
+    im = im.resize((target_width,target_height),Image.ANTIALIAS)
+    pixels = np.asarray(im.getdata())
+    #pixels = pixels/255.0*2 - 1
+    pixels = np.resize(pixels, (im.height, im.width, 3, 1)) #need the extra dimension to stack later
+    return im, pixels
 
-X_test = np.empty(shape=(target_width,target_height,3,0)) #needs optimization
-for fname in glob.glob(path):
-	im, pixels = load_image(fname)
-	X_test = np.append(X_test, pixels, axis=3)
-	numImages = numImages + 1
-
-X_train = X_test[:,:,:,int(numImages*random.random())]
-
-# Model Architecture
-model = Sequential()
-model.add(Convolution2D(32, (3, 3), activation='relu', input_shape=(img_width,img_height,img_depth)))
-model.add(Convolution2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
- 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Dense(2))
-
-count = 0
-overallMax = 0
-overallImg = 0
-list = [None]*lim
-
-# Compile model
-model.compile(loss='mean_squared_error',
-			  optimizer='adam',
-			  metrics=['accuracy'])
+def display(img):
+    return Image.fromarray(img.squeeze().astype('uint8'), 'RGB')
  
 # Fit model on training data
 def train(X,Y, e):
@@ -71,12 +40,45 @@ def train(X,Y, e):
 
 @app.route('/',methods=['GET','POST'])
 def index():
+    # Machine Learning Witchcraft
+    
+    # grab file dimensions
+    im,_ = load_image('scraped-sofas/sofa_0.jpg')
+
+    img_width = im.width
+    img_height = im.height
+    
+    # Compile model
+    model.compile(loss='mean_squared_error',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    X_test = np.empty(shape=(target_width,target_height,3,0)) #needs optimization
+    for fname in glob.glob(path):
+        im, pixels = load_image(fname)
+        X_test = np.append(X_test, pixels, axis=3)
+        numImages = numImages + 1
+
+    X_train = X_test[:,:,:,int(numImages*random.random())]
+
+    # Model Architecture
+    model = Sequential()
+    model.add(Convolution2D(32, (3, 3), activation='relu', input_shape=(img_width,img_height,img_depth)))
+    model.add(Convolution2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Dense(2))
+
+    count = 0
+    overallMax = 0
+    overallImg = 0
+    list = [None]*lim
     return render_template('sofa.html')
 
 @app.route('/submission/<num>', methods=['POST'])
-def rating(num=-1):
-	if num == -1:
-		break
+def rating():
 	if count<lim:
 		return display(X_train)
 		Y_train = np.full((1,1),num)
