f261ed8273f683eb4cc9bf32b4c0eccd0c6897ab
Lawrence Pang <lawrencepang36@gmail.com>
2017-09-16 09:24:22 -0400

    fix more bugs

--- ml.py
+++ ml.py
@@ -9,38 +9,42 @@ from PIL import Image
 import glob
 
 path = "sofa-folder/*.jpg"
+target_width = 64
+target_height = 64
 
 #Returns pixel data as well as width 
 def load_image( filename ):
-    im = Image.open( filename )
-    pixels = np.asarray(im.getdata())
-    #pixels = pixels/255.0*2 - 1
-    pixels = np.resize(pixels, (im.height, im.width, 3, 1)) #need the extra dimension to stack later
-    return im, pixels
+	im = Image.open( filename )
+	im = im.resize((target_width,target_height),Image.ANTIALIAS)
+	pixels = np.asarray(im.getdata())
+	#pixels = pixels/255.0*2 - 1
+	pixels = np.resize(pixels, (im.height, im.width, 3, 1)) #need the extra dimension to stack later
+	return im, pixels
 
 im, X_test = load_image('sofa-folder/sofa.jpg')
 
 img_width = im.width
 img_height = im.height
+print(img_width)
+print(img_height)
 img_depth = 3
 threshold = 0.9
 lim = 20
 numImages = 0
 
 def display(img):
-    #img = (1+img)*255/2
-    print(img.shape)    
-    new = Image.fromarray(img.squeeze().astype('uint8'), 'RGB')
-    new.save('out.png')
-    new.show()
+	#img = (1+img)*255/2
+	print(img.shape)    
+	new = Image.fromarray(img.squeeze().astype('uint8'), 'RGB')
+	new.save('out.png')
+	new.show()
 
 for fname in glob.glob(path):
-    im, pixels = load_image(fname)
-    X_test = np.append(X_test, pixels, axis=3)
-    numImages = numImages + 1
+	im, pixels = load_image(fname)
+	X_test = np.append(X_test, pixels, axis=3)
+	numImages = numImages + 1
 
 X_train = X_test[:,:,:,int(numImages*random.random())]
-
 
 # Model Architecture
 model = Sequential()
@@ -53,34 +57,34 @@ model.add(Dense(128))
 model.add(Dense(1))
 
 # Compile model
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model.compile(loss='mean_squared_error',
+			  optimizer='adam',
+			  metrics=['accuracy'])
  
 # Fit model on training data
 def train(X,Y):
-    model.fit(X, Y, batch_size=1, nb_epoch=3, verbose=1)
+	model.fit(X, Y, batch_size=1, nb_epoch=100)
 
 count = 0
 overallMax = 0
 overallImg = 0
 
 while count<lim:
-    display(X_train)
-    Y_train = np.empty([1,1])
-    np[0, 0] = input('Score: ')
-    train(X_train,Y_train)
-    max_thing = 0
-    for img in X_test:
-        score = model.evaluate(img, verbose=0)
-        if score>max_thing:
-            max_thing = score
-            X_train = img
-
-        if score>overallMax:
-            overallMax = score
-            overallImg = img
-    if score>threshold:
-        display(X_train)
+	display(X_train)
+	Y_train = np.full((1,1),input('Score: '))
+	train(X_train[np.newaxis,...],Y_train)
+	max_thing = 0
+	for i in range(0,X_test.shape[3]):
+		img = X_test[:,:,:,i]
+		print(img.shape)
+		score = model.evaluate(img[np.newaxis,...], np.zeros((1,1)), verbose=1)
+		if score>max_thing:
+			max_thing = score
+			X_train = img
+		if score>overallMax:
+			overallMax = score
+			overallImg = img
+	if score>threshold:
+		display(X_train)
 
 display(overallImg)
