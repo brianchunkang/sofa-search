8e275d1533738441047e34dcc701ea5e50432e98
VictorHYu <victor.yu.canada@gmail.com>
2017-09-16 03:26:22 -0400

    prepare for heroku deployment

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def static_page():
+    return app.send_static_file('sofa.html')
+
+if __name__ == '__main__':
+    app.run()
