f2f7d40267477cae37478c59a16cb69259dc6faa
VictorHYu <victor.yu.canada@gmail.com>
2017-09-16 09:36:28 -0400

    add image page

--- static/scripts.js
+++ static/scripts.js
@@ -1,38 +1,52 @@
 $(document).ready(function() {
+                  
+    var transitionDelay1 = 500;
+    var transitionDelay2 = 500;
+                  
     $("#buttonAbout").click(function() {
-        //hide welcome stuff
+        
         $("#welcomeContent").css("opacity","0");
                             
         setTimeout(function() {
             $("#welcomeContent").addClass("hidden");
-            //show about
             $(".overlay-white").addClass("expanded-medium overlay-whiter");
             $("#aboutContent").removeClass("hidden");
                    
             setTimeout(function() {
                 $("#aboutContent").css("opacity","1");
-            }, 800);
-        }, 500);
-    });
-                  
-    $("#buttonBack").click(function() {
-    
-    //hide about stuff
-    $("#aboutContent").css("opacity","0");
-
-        setTimeout(function() {
-            $("#aboutContent").addClass("hidden");
-            //show about
-            $(".overlay-white").removeClass("expanded-medium overlay-whiter");
-            $("#welcomeContent").removeClass("hidden");
-                   
-            setTimeout(function() {
-                $("#welcomeContent").css("opacity","1");
-            }, 800);
-        }, 500);
+            }, transitionDelay1);
+        }, transitionDelay2);
     });
                   
     $("#buttonStart").click(function() {
-        //show start
+                            
+        $("#welcomeContent").css("opacity","0");
+        
+        setTimeout(function() {
+            $("#welcomeContent").addClass("hidden");
+            $(".overlay-white").addClass("expanded-full");
+            $("#sofaContent").removeClass("hidden");
+                   
+            setTimeout(function() {
+                $("#sofaContent").css("opacity","1");
+            }, transitionDelay1);
+        }, transitionDelay2);
+    });
+                  
+    $(".btn-back").click(function() {
+       
+        $("#aboutContent").css("opacity","0");
+        $("#sofaContent").css("opacity","0");
+
+        setTimeout(function() {
+        $("#aboutContent").addClass("hidden");
+        $("#sofaContent").addClass("hidden");
+        $(".overlay-white").removeClass("expanded-medium expanded-full overlay-whiter");
+        $("#welcomeContent").removeClass("hidden");
+
+        setTimeout(function() {
+            $("#welcomeContent").css("opacity","1");
+            }, transitionDelay1);
+        }, transitionDelay2);
     });
 });
